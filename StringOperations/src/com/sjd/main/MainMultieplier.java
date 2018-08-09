cf82849d7269ea912ca1cffb56832df6c0e24d8a
Vova <Vova@192.168.0.102>
2018-08-09 17:55:14 +0300

    fix variables name

--- StringOperations/src/com/sjd/main/MainMultieplier.java
+++ StringOperations/src/com/sjd/main/MainMultieplier.java
@@ -4,30 +4,30 @@ public class MainMultieplier {
 
 	public static void main(String[] args) {
 		
-		int a = 111;
-		int b = 512;
-		String s = null;
-		String c = " ";
+		int multieplier1 = 111;
+		int multieplier2 = 512;
+		String emptyValuecontainer = null;
+		String emptyValue = " ";
 		
-		String num = Integer.toString(b);
+		String num = Integer.toString(multieplier2);
 		char [] chars = num.toCharArray();
 		char [] empty = num.toCharArray();
 		for(int j = 0 ; j < empty.length; j ++ ) {
-			empty[j] = c.charAt(0);
-			s = new String(empty);
+			empty[j] = emptyValue.charAt(0);
+			emptyValuecontainer = new String(empty);
 		}
 		
 		for(int i = chars.length -1 ;i >=0;i--) {	
-			int num1 = Character.getNumericValue(chars[i]);
-			int result = a * num1;
-			String d = s.substring(0, i);
+			int reverseValue = Character.getNumericValue(chars[i]);
+			int result = multieplier1 * reverseValue;
+			String d = emptyValuecontainer.substring(0, i);
 			System.out.println(" " + d + result);
 			if(i>0) {
 				System.out.println("+");
 			}else System.out.println("------");
 		}
 		
-		System.out.println(" " + a*b);
+		System.out.println(" " + multieplier1*multieplier2);
 
 	}
 
